dadb05d0f6f4ed152f43312566533889aef8d832
Jagruti <jagrutimeniya245@gmail.com>
2021-03-28 16:59:02 +0530

    Faculty Bugs

---
+++ GECP/GECP/Models/Pages/FacultyPageModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GECP.Models.Pages
+{
+    public class FacultyPageModel
+    {
+        public FacultyDetailsModel FacultyDetailsModel { get; set; }
+        public IList<FacultyDetailsModel> FacultyDetailsModels { get; set; }
+
+        public FacultyPageModel()
+        {
+            FacultyDetailsModels = new List<FacultyDetailsModel>();
+        }
+    }
+}
